7f4081a5473a22ab6a2d5d0a8a481d549891b1bc
Koljane <koljane@gmail.com>
2016-11-04 09:26:59 +0100

    Initial Commit

---
+++ SnapAttack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapAttack
+//
+//  Created by Konstantin Nenadov on 11/4/16.
+//  Copyright © 2016 Konstantin Nenadov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
